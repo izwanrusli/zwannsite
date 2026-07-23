a5525d63600913402fc3482325d4778cb8bd9f1c
Mohamad Nor Izwan Rusli <izwan.rusli@shareinvestor.com>
2026-07-23 14:51:22 +0800

    fix: drop www from LinkedIn URL

--- src/data/socials.js
+++ src/data/socials.js
@@ -4,7 +4,7 @@
 const socials = {
   email: 'izwanrusli8@gmail.com',
   github: 'https://github.com/izwanrusli',
-  linkedin: 'https://www.linkedin.com/in/izwan-rusli',
+  linkedin: 'https://linkedin.com/in/izwan-rusli',
   resume: '/izwan-resume-2025.pdf',
 };
 
